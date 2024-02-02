77fb6f6df284b2b27af1e7280a41c91c81734422
Eduardo Becerra <sistemas1@grupomunoz.mx>
2024-02-02 10:17:46 -0600

    Steps 1 to 9 of the course Added composable functions (cards & screen) and previews to interact with the UI

--- app/src/main/java/com/edebec/basicsconceptscodelab/MainActivity.kt
+++ app/src/main/java/com/edebec/basicsconceptscodelab/MainActivity.kt
@@ -3,13 +3,25 @@ package com.edebec.basicsconceptscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.edebec.basicsconceptscodelab.ui.theme.BasicsConceptsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,19 +39,85 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = MaterialTheme.colorScheme.primary) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
+fun OnboardingScreen(modifier: Modifier = Modifier, onContinueClicked: () -> Unit) {
+    Column(modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "Welcome to the Basics Codelab!")
+        Button(onClick = onContinueClicked, modifier = Modifier.padding(vertical = 24.dp)) {
+            Text(text = "Continue")
+        }
     }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+    var shouldShowOnboarding by remember {
+        mutableStateOf(true)
+    }
+    Surface(modifier) {
+        if (shouldShowOnboarding) {
+            OnboardingScreen {
+                shouldShowOnboarding = true
+            }
+        } else {
+            Greetings()
+        }
+    }
+}
+
+@Composable
+fun Greetings(modifier: Modifier = Modifier, names: List<String> = listOf("Jesus", "Eduardo")) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    Surface(modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp), color = MaterialTheme.colorScheme.primary) {
+        Row(modifier = modifier.padding(24.dp)) {
+            Column(
+                modifier = modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(
+                    text = "Hello"
+                )
+                Text(
+                    text = "$name!"
+                )
+            }
+            ElevatedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(text = if (expanded.value) "Show less" else "Show more")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MyAppPreview(modifier: Modifier = Modifier) {
+    BasicsConceptsCodelabTheme {
+        MyApp(modifier = Modifier.fillMaxSize())
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    BasicsConceptsCodelabTheme {
+        OnboardingScreen(onContinueClicked = {})
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
     BasicsConceptsCodelabTheme {
-        Greeting("Android")
+        Greetings()
     }
 }
